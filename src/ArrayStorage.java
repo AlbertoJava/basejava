ac9188c590cdbf8a12cb63fed57f78facd01b9c5
Albert <alblist@list.ru>
2019-07-21 16:40:01 +0300

    First lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,50 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int lastRecord=0;
 
     void clear() {
+        lastRecord=0;
     }
 
     void save(Resume r) {
+        storage[lastRecord++]=r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result=null;
+        for (int i=0;i<lastRecord;i++){
+            if (storage[i].uuid.equals(uuid)){
+                result=storage[i];
+                break;
+            }
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        for (int i=0;i<lastRecord;i++){
+            if (storage[i].uuid.equals(uuid)){
+                storage[i]=storage[lastRecord-1];
+                storage[lastRecord-1]=null;
+                lastRecord--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage,0,lastRecord);
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
