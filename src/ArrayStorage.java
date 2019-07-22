efa68b165abb0db737341b098e2b2c9efae682c4
Alb <alblist@list.ru>
2019-07-22 10:54:06 +0300

    Lesson 1, debugged mentors notes.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,32 +5,33 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int lastRecord=0;
+    private int lastRecord = 0;
 
     void clear() {
-        lastRecord=0;
+        for (int i = 0; i < lastRecord; i++) {
+            storage[i] = null;
+        }
+        lastRecord = 0;
     }
 
     void save(Resume r) {
-        storage[lastRecord++]=r;
+        storage[lastRecord++] = r;
     }
 
     Resume get(String uuid) {
-        Resume result=null;
-        for (int i=0;i<lastRecord;i++){
-            if (storage[i].uuid.equals(uuid)){
-                result=storage[i];
-                break;
+        for (int i = 0; i < lastRecord; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
             }
         }
-        return result;
+        return null;
     }
 
     void delete(String uuid) {
-        for (int i=0;i<lastRecord;i++){
-            if (storage[i].uuid.equals(uuid)){
-                storage[i]=storage[lastRecord-1];
-                storage[lastRecord-1]=null;
+        for (int i = 0; i < lastRecord; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[lastRecord - 1];
+                storage[lastRecord - 1] = null;
                 lastRecord--;
                 break;
             }
@@ -41,10 +42,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage,0,lastRecord);
+        return Arrays.copyOfRange(storage, 0, lastRecord);
     }
 
     int size() {
-        return storage.length;
+        return lastRecord;
     }
 }
