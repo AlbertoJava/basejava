c4e3506463dca4e4577eb96a215da7ad48d1a68e
Albert <alblist@list.ru>
2019-07-25 00:27:04 +0300

    Second lesson

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,46 +6,76 @@ import java.util.Arrays;
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     private int lastRecord = 0;
+    private static final String ERROR_MSG = "ERROR: record doesn't exists!";
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < lastRecord; i++) {
             storage[i] = null;
         }
         lastRecord = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        if (get(r) != null) {
+            System.out.println(ERROR_MSG);
+            return;
+        }
         storage[lastRecord++] = r;
     }
 
-    Resume get(String uuid) {
-        for (int i = 0; i < lastRecord; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
-            }
+    public Resume get(String uuid) {
+        if (getNumber(uuid) >= 0) {
+            return storage[getNumber(uuid)];
         }
         return null;
     }
 
-    void delete(String uuid) {
+    private Resume get(Resume resume) {
+        return get(resume.uuid);
+    }
+
+    private int getNumber(String uuid) {
         for (int i = 0; i < lastRecord; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[lastRecord - 1];
-                storage[lastRecord - 1] = null;
-                lastRecord--;
-                break;
+                return i;
             }
         }
+        return -1;
     }
+
+
+    public void delete(String uuid) {
+        if (getNumber(uuid) >= 0) {
+            storage[getNumber(uuid)] = storage[lastRecord - 1];
+            storage[lastRecord - 1] = null;
+            lastRecord--;
+        } else {
+            System.out.println(ERROR_MSG);
+        }
+    }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+
+
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, lastRecord);
     }
 
-    int size() {
+    public int size() {
         return lastRecord;
+    }
+
+
+    public boolean update(Resume resume) {
+        int pos = getNumber(resume.uuid);
+        if (pos == -1) {
+            System.out.println(ERROR_MSG);
+            return false;
+        }
+        get(storage[pos]).uuid = resume.uuid;
+        return true;
     }
 }
