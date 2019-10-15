0c220c10f339bd91add20f4245f0870b7b3a9cb3
basejava <alblist@list.ru>
2019-10-15 12:07:03 +0300

    lesson 12 util.minValue исправление ошибок

--- src/com/urise/webapp/util/minValue.java
+++ src/com/urise/webapp/util/minValue.java
@@ -1,18 +1,25 @@
 package com.urise.webapp.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class minValue {
     public static void main(String[] args) {
         List<Integer> values = new ArrayList<>();
-        for (int i=0;i<10;i++){
-            values.add (i);
+        for (int i = 1; i < 10; i++) {
+            values.add((int) (Math.random()*10));
+            //values.add(i);
         }
-        System.out.println("Sum of elements =" + values.stream().reduce((x1,x2)->x1+x2).get());
-        printStream(oddOrEven(values).stream(),"List without streams");
-        printStream(oddOrEvenOptional(values).stream(), "List created using stream line!" );
+
+        printStream(values.stream(), "Source data");
+        System.out.println("Sum of elements =" + values.stream().reduce((x1, x2) -> x1 + x2).get());
+        System.out.println("Min number = " + minValue(values.toArray(new Integer[9])));
+        printStream(oddOrEven(values).stream(), "List without streams");
+        printStream(oddOrEvenOptional(values).stream(), "List created using stream line!");
 
     }
 
@@ -22,35 +29,37 @@ public class minValue {
         System.out.println("------------");
     }
 
-    public static int minValue(int[] values) {
-        return Arrays.stream(values).min().getAsInt();
+    public static int minValue(Integer[] values) {
+        List<Integer> list = Arrays.stream(values).distinct().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        int result = 0;
+        for (Integer i = 0; i < list.size(); i++) {
+            result += list.get(i) * Math.pow(10, i);
+        }
+        return result;
     }
 
 
-    static List<Integer> oddOrEven(List<Integer> integers){
+    static List<Integer> oddOrEven(List<Integer> integers) {
         List<Integer> evenList = new ArrayList<>();
         List<Integer> oddList = new ArrayList<>();
-        int sum=0;
-        for (int i=0;i<integers.size();i++){
-            sum=+integers.get(i);
-            if (i%2==0){
-                oddList.add(integers.get(i));
-            }
-            else {
-                evenList.add(integers.get(i));
+        int sum = 0;
+        for (int i = 0; i < integers.size(); i++) {
+            int elem = integers.get(i);
+            sum = +elem;
+            if (elem % 2 == 0) {
+                oddList.add(elem);
+            } else {
+                evenList.add(elem);
             }
         }
-        return sum%2==0?evenList:oddList;
+        return sum % 2 == 0 ?  oddList:evenList;
     }
 
-    static List<Integer> oddOrEvenOptional(List<Integer> integers){
-        //Map<Boolean, List<Integer>> result
-        List<Integer> result=integers.stream()
-                  .collect(Collectors.partitioningBy((x)-> integers.indexOf(x)%2==0))
-                  .get(integers.stream().reduce((x1,x2)->x1+x2).get()%2!=0)
-                  ;
+    static List<Integer> oddOrEvenOptional(List<Integer> integers) {
 
-          return result;
+        return integers.stream()
+                .collect(Collectors.partitioningBy((x) -> x % 2 == 0))
+                .get(integers.stream().reduce((x1, x2) -> x1 + x2).get() % 2 != 0);
     }
 
 }
