937dad2f95f6836fcedfd49c4f1550c088fb2bfb
basejava <alblist@list.ru>
2019-10-16 21:24:09 +0300

    lesson 12 util.minValue исправление ошибок

--- src/com/urise/webapp/util/minValue.java
+++ src/com/urise/webapp/util/minValue.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,10 +13,8 @@ public class minValue {
     public static void main(String[] args) {
         List<Integer> values = new ArrayList<>();
         for (int i = 1; i < 10; i++) {
-            values.add((int) (Math.random()*10));
-            //values.add(i);
-        }
-
+            values.add((int) (Math.random() * 10));
+         }
         printStream(values.stream(), "Source data");
         System.out.println("Sum of elements =" + values.stream().reduce((x1, x2) -> x1 + x2).get());
         System.out.println("Min number = " + minValue(values.toArray(new Integer[9])));
@@ -26,18 +26,18 @@ public class minValue {
     private static void printStream(Stream stream, String title) {
         System.out.println("------" + title + "------");
         stream.forEach(System.out::println);
-        System.out.println("------------");
+        System.out.println("-------------------------");
     }
 
     public static int minValue(Integer[] values) {
-        List<Integer> list = Arrays.stream(values).distinct().sorted(Collections.reverseOrder()).collect(Collectors.toList());
-        int result = 0;
-        for (Integer i = 0; i < list.size(); i++) {
-            result += list.get(i) * Math.pow(10, i);
-        }
-        return result;
-    }
+        AtomicInteger x = new AtomicInteger();
+        return (int) Arrays.stream(values)
+                .distinct()
+                .sorted(Collections.reverseOrder())
+                .map((Function<Integer, Object>) elem -> elem * (int) Math.pow(10, x.getAndIncrement()))
+                .reduce((z, y) -> (Integer) z + (Integer) y).orElse(0);
 
+    }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
         List<Integer> evenList = new ArrayList<>();
@@ -52,14 +52,14 @@ public class minValue {
                 evenList.add(elem);
             }
         }
-        return sum % 2 == 0 ?  oddList:evenList;
+        return sum % 2 == 0 ? oddList : evenList;
     }
 
     static List<Integer> oddOrEvenOptional(List<Integer> integers) {
 
         return integers.stream()
                 .collect(Collectors.partitioningBy((x) -> x % 2 == 0))
-                .get(integers.stream().reduce((x1, x2) -> x1 + x2).get() % 2 != 0);
+                .getOrDefault(integers.stream().reduce((x1, x2) -> x1 + x2).get() % 2 != 0, new ArrayList<>());
     }
 
 }
