1b7fb92b3ae86ce7e7ecc716c5d2100315cdc991
basejava <alblist@list.ru>
2019-10-21 12:14:38 +0300

    lesson 13 dz update init_db.sql

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,22 +1,13 @@
-create table if not exists resume
-(
-  uuid      char(36) not null
-    constraint resume_pkey
-    primary key,
-  full_name text     not null
+CREATE TABLE resume (
+  uuid      CHAR(36) PRIMARY KEY NOT NULL,
+  full_name TEXT                 NOT NULL
 );
-alter table resume
-  owner to postgres;
-create table if not exists contact
-(
-  id          serial   not null
-    constraint contact_pkey
-    primary key,
-  resume_uuid char(36) not null references resume (uuid) on delete cascade,
-  type        text     not null,
-  value       text     not null
+
+CREATE TABLE contact (
+  id          SERIAL,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
 );
-alter table contact
-  owner to postgres;
-create unique index if not exists contact__uuid_type_index
-  on contact (resume_uuid, type);
+CREATE UNIQUE INDEX contact_uuid_type_index
+  ON contact (resume_uuid, type);
